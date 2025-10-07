22d15dbd5f960dccf96f92bf8a9ee8cbb6965a97
LoanDAM <141652400+userLoan@users.noreply.github.com>
2025-10-08 00:32:11 +0700

    Update App.js

--- gateway/src/App.js
+++ gateway/src/App.js
@@ -1,299 +1,325 @@
 import React, { Component } from 'react';
-import Web3 from 'web3'
-import Nav from './Components/Nav'
-import Description from './Components/Description'
-import Container from './Components/Container'
-import Shoes from './Items/all'
+import Web3 from 'web3';
+import Nav from './Components/Nav';
+import Description from './Components/Description';
+import Container from './Components/Container';
+import Shoes from './Items/all';
 
+// =============== Helper: fetch có timeout ===================
+function fetchWithTimeout(url, options, ms) {
+  if (typeof ms !== 'number') ms = 10000; // mặc định 10s
+  return Promise.race([
+    fetch(url, options || {}),
+    new Promise((_, reject) =>
+      setTimeout(() => reject(new Error('Timeout after ' + ms + 'ms')), ms)
+    ),
+  ]);
+}
 
-
+// =============== MAIN APP ===================================
 class App extends Component {
-    constructor(){
-        super();
-		this.appName = 'Sindbad Commerce';
-        this.shoes = Shoes;
-		this.newPayment = this.newPayment.bind(this);
-		this.closePayment = this.closePayment.bind(this);
-		this.PaymentWait = this.PaymentWait.bind(this);
-		this.tick = this.tick.bind(this);
-		this.bCheck = this.bCheck.bind(this);
-		this.startTimer = this.startTimer.bind(this);
-		
-		this.state = {
-			shoes: [],
-			PaymentDetail: {},
-			Conv: 300,
-			defaultGasPrice: null,
-            defaultGasLimit: 200000,
-			paymentf: false,
-			mAddress: '0x',
-			amount: 0,
-			diff: 0,
-		    seconds: '00',   // responsible for the seconds 
-            minutes: '15',  // responsible for the minutes
-			tflag: true
-		};
-		
-		
-			}
-  
-    newPayment = (index) => {
-		
-		var mAddress;
-		let app = this;
-		(async function  main(){
-		await fetch('http://127.0.0.1:5000/api/getMAddress')
-		.then(response => response.json())
-		.then(data => {
-			  			mAddress = data.MAddress;
-			console.log(mAddress);
-        app.setState({
-            PaymentDetail: app.state.shoes[index],
-			mAddress
-        })
-						
-					  });
-		
-		var Conv;
-	await fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD')
-		.then(response => response.json())
-		.then(data => {
-			  			Conv=data.USD;
-						
-			app.setState({
-                        Conv       
-						})
-					  });
-		})();	
-    };	
+  constructor() {
+    super();
+    this.appName = 'Sindbad Commerce';
+    this.shoes = Shoes;
 
-closePayment = () => {
-        	
-			clearInterval(this.intervalHandle);
-			clearInterval(this.intervalBalance);	
+    this.closePayment = this.closePayment.bind(this);
+    this.PaymentWait = this.PaymentWait.bind(this);
+    this.resetApp = this.resetApp.bind(this);
+    this.bCheck = this.bCheck.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.tick = this.tick.bind(this);
 
-			this.setState({
-            PaymentDetail: {},
-			paymentf: false,	
-			mAddress: '0x',
-			amount: 0,
-			diff: 0,
-		    seconds: '00',   // responsible for the seconds 
-            minutes: '15',  // responsible for the minutes
-			tflag: true,
-			defaultGasPrice: null,
-            defaultGasLimit: 200000	
-	})
-};
-
-PaymentWait = (mAddress,amount) => {
-			
-			this.setState({
-            paymentf: true,
-			amount,
-			mAddress	
-        })
-		
-	};
-
-resetApp = () => {
-      this.setState({
-            PaymentDetail: {},
-			paymentf: false,	
-			mAddress: '0x',
-			amount: 0,
-			diff: 0,
-		    seconds: '00',   // responsible for the seconds 
-            minutes: '15',  // responsible for the minutes
-			tflag: true,
-			defaultGasPrice: null,
-            defaultGasLimit: 200000	
-	})    
-};
-
-
-setGasPrice = (web3) => {
-        web3.eth.getGasPrice((err,price) => {if(!err)
-		{
-			console.log(price);	
-            price = web3.utils.fromWei(price.toString(),'gwei');
-            this.setState({defaultGasPrice: price})
-		}
-		else
-		{
-			console.log(err);
-		}										  
-        });
+    this.state = {
+      shoes: [],
+      PaymentDetail: {},
+      Conv: 300, // tỷ giá USD mặc định
+      defaultGasPrice: null,
+      defaultGasLimit: 200000,
+      paymentf: false,
+      mAddress: '0x',
+      amount: 0,
+      diff: 0,
+      seconds: '00',
+      minutes: '15',
+      tflag: true,
+      errorMsg: null,
     };
+  }
 
-
-MMaskTransfer = (MRAddress,amount) => {
-    
-	let app = this; 
-	if (window.ethereum) {
-        const ethereum = window.ethereum;
-       	let web3 = new Web3(ethereum);
-		ethereum.enable().then((accounts) => {
-		let account = accounts[0];
-		 web3.eth.defaultAccount = account ;
-		this.setGasPrice(web3);
-			let tAmount = amount * 1000000000000000000; 
-		let transObj = {to: MRAddress, gas: this.state.defaultGasLimit,gasPrice: this.state.defaultGasPrice, value: tAmount}	
- 		web3.eth.sendTransaction(transObj,function (error, result){  
-			if(!error){
-                    console.log(result);
-					app.resetApp();
-                } else{
-                    console.log(error);
-                }
-			});
-			
-});
-}
-}
-
-tick(){		
-var min = Math.floor(this.secondsRemaining / 60);
-var sec = this.secondsRemaining - (min * 60);
-
-this.setState({
-  minutes: min,
-  seconds: sec
-})
-
-if (sec < 10) {
-  this.setState({
-    seconds: "0" + this.state.seconds,
-  })
-}
-if (min < 10) {
-this.setState({
-  minutes: "0" + this.state.minutes,
- })
-}
-if (min === 0 & sec === 0) {
-clearInterval(this.intervalHandle);
-clearInterval(this.intervalBalance);	
-}	
-this.secondsRemaining--;
-}
-
-
-
-bCheck(){
-let app = this;	
-let amount = this.state.amount;
-let intervalHandle = this.intervalHandle;
-let intervalBalance = this.intervalBalance;	
-this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));	
-this.web3.eth.getBalance(this.state.mAddress,function (error, result){  
-			if(!error){
-				let diff = result / 1000000000000000000;
-								if(diff >= amount )
-				{					
-			clearInterval(intervalHandle);
-			clearInterval(intervalBalance);
-		
-				}
-				
-				app.setState ({
-				diff	
-  				})
-			}
-				
-		else
-		{
-			console.log(error);
-		}
-	
-			});
-}
-	
-
-
-startTimer = () =>{
-
-if(this.state.tflag == true)
-{
-this.intervalHandle = setInterval(this.tick,1000);
-this.intervalBalance = setInterval(this.bCheck,10000);
-	
-	let time = this.state.minutes;
-	this.secondsRemaining = time * 60;
-	this.setState({
-  tflag: false
- });
-}
-}	
-
-
-
-
-
-
-		
-    componentDidMount() {
-		
-		let app = this;
-		let shoes = app.state.shoes;
-	
-		
-		
-		Shoes.forEach((shoe) => {
-		let logo = shoe.logo;
-        let price = shoe.price;
-        let image = shoe.image;
-        let name = shoe.name;
-		
-		
-			
-		shoes.push({
-                        logo,
-                        price,
-                        name,
-                        image,
-                    });
-		
-		app.setState({
-                        shoes
-       
-		})
-	});
-		
-		
-	}
-		
-    render() {
-               
-                return (
-                    <div>
-                        <Nav appName={this.appName} />
-						<div>                        </div>
-						<Description />
-						<Container 
-							shoes={this.state.shoes}
-							newPayment={this.newPayment}
-							closePayment={this.closePayment}
-							PaymentDetail={this.state.PaymentDetail}
-							mAddress={this.state.mAddress}
-							amount={this.state.amount}
-							diff={this.state.diff}
-							paymentf={this.state.paymentf}
-							Conv={this.state.Conv}
-							MMaskTransfer={this.MMaskTransfer}
-							PaymentWait={this.PaymentWait}
-							startTimer={this.startTimer}
-							tick={this.tick}
-							privateToAddress={this.privateToAddress}
-							getRandomWallet={this.getRandomWallet}
-							defaultGasPrice={this.state.defaultGasPrice}
-                            defaultGasLimit={this.state.defaultGasLimit}
-							minutes={this.state.minutes}
-        					seconds={this.state.seconds}/>
-									</div>
-                		)
-           
-        }
+  // =============== Lấy địa chỉ merchant và tỷ giá ===================
+  newPayment = async (index) => {
+    if (index == null || !this.state.shoes[index]) {
+      this.setState({ errorMsg: 'Sản phẩm không hợp lệ.' });
+      return;
     }
+
+    try {
+      // 1️⃣ Gọi API backend để lấy địa chỉ merchant
+      const addrRes = await fetchWithTimeout(
+        'http://127.0.0.1:5000/api/getMAddress',
+        {},
+        10000
+      );
+      if (!addrRes.ok)
+        throw new Error(`API ${addrRes.status} ${addrRes.statusText}`);
+      const addrJson = await addrRes.json();
+      const mAddr = addrJson.MAddress || addrJson.mAddress || addrJson.address;
+
+      if (!mAddr || !mAddr.startsWith('0x') || mAddr.length < 10) {
+        throw new Error('Địa chỉ ví merchant không hợp lệ.');
+      }
+
+      // 2️⃣ Lấy tỷ giá ETH → USD
+      const rateRes = await fetchWithTimeout(
+        'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD',
+        {},
+        10000
+      );
+      if (!rateRes.ok)
+        throw new Error(`Rate ${rateRes.status} ${rateRes.statusText}`);
+      const rateJson = await rateRes.json();
+      const conv = rateJson.USD || 300;
+
+      // 3️⃣ Cập nhật state
+      this.setState({
+        PaymentDetail: this.state.shoes[index],
+        mAddress: mAddr,
+        Conv: conv,
+        errorMsg: null,
+      });
+      console.log('✅ Fetched merchant address:', mAddr);
+    } catch (err) {
+      console.error('❌ newPayment failed:', err);
+      this.setState({
+        PaymentDetail: {},
+        mAddress: '0x',
+        errorMsg:
+          err && err.message
+            ? 'Không gọi được API: ' + err.message
+            : 'Không gọi được API backend.',
+      });
+    }
+  };
+
+  // =============== Đóng giao dịch ===================
+  closePayment() {
+    clearInterval(this.intervalHandle);
+    clearInterval(this.intervalBalance);
+
+    this.setState({
+      PaymentDetail: {},
+      paymentf: false,
+      mAddress: '0x',
+      amount: 0,
+      diff: 0,
+      seconds: '00',
+      minutes: '15',
+      tflag: true,
+      defaultGasPrice: null,
+      defaultGasLimit: 200000,
+      errorMsg: null,
+    });
+  }
+
+  // =============== Bắt đầu chờ thanh toán ===================
+  PaymentWait(mAddress, amount) {
+    this.setState({
+      paymentf: true,
+      amount: amount,
+      mAddress: mAddress,
+    });
+  }
+
+  resetApp() {
+    this.closePayment();
+  }
+
+  // =============== Lấy Gas Price ===================
+  setGasPrice = (web3) => {
+    web3.eth.getGasPrice((err, priceWei) => {
+      if (!err) {
+        this.setState({ defaultGasPrice: priceWei.toString() });
+      } else {
+        console.error('GasPrice error:', err);
+      }
+    });
+  };
+
+  // =============== Gửi giao dịch qua MetaMask ===================
+  MMaskTransfer = (MRAddress, amount) => {
+    const app = this;
+
+    if (!window.ethereum) {
+      this.setState({
+        errorMsg: 'Không phát hiện ví MetaMask trên trình duyệt.',
+      });
+      return;
+    }
+
+    const ethereum = window.ethereum;
+    const web3 = new Web3(ethereum);
+
+    ethereum
+      .request({ method: 'eth_requestAccounts' })
+      .then((accounts) => {
+        const account = accounts[0];
+        web3.eth.defaultAccount = account;
+
+        if (!app.state.defaultGasPrice) {
+          app.setGasPrice(web3);
+        }
+
+        let valueWei = '0';
+        try {
+          valueWei = web3.utils.toWei(String(amount), 'ether');
+        } catch (e) {
+          app.setState({ errorMsg: 'Số tiền không hợp lệ.' });
+          return;
+        }
+
+        const tx = {
+          from: account,
+          to: MRAddress,
+          gas: app.state.defaultGasLimit,
+          gasPrice: app.state.defaultGasPrice || undefined,
+          value: valueWei,
+        };
+
+        web3.eth.sendTransaction(tx, (error, result) => {
+          if (!error) {
+            console.log('✅ Tx hash:', result);
+            app.resetApp();
+          } else {
+            console.error('❌ Tx error:', error);
+            app.setState({
+              errorMsg:
+                error && error.message
+                  ? error.message
+                  : 'Gửi giao dịch thất bại.',
+            });
+          }
+        });
+      })
+      .catch((err) => {
+        console.error('MetaMask request error:', err);
+        app.setState({
+          errorMsg: 'Không thể truy cập tài khoản MetaMask.',
+        });
+      });
+  };
+
+  // =============== Đồng hồ đếm lùi ===================
+  tick() {
+    let min = Math.floor(this.secondsRemaining / 60);
+    let sec = this.secondsRemaining - min * 60;
+
+    this.setState({
+      minutes: min < 10 ? '0' + min : String(min),
+      seconds: sec < 10 ? '0' + sec : String(sec),
+    });
+
+    if (min === 0 && sec === 0) {
+      clearInterval(this.intervalHandle);
+      clearInterval(this.intervalBalance);
+    }
+
+    this.secondsRemaining = Math.max(0, this.secondsRemaining - 1);
+  }
+
+  // =============== Kiểm tra số dư ví merchant ===================
+  bCheck() {
+    let app = this;
+    let amount = this.state.amount;
+    let mAddr = this.state.mAddress;
+
+    // ✅ Bỏ qua nếu chưa có địa chỉ hợp lệ
+    if (!mAddr || !/^0x[a-fA-F0-9]{40}$/.test(mAddr)) {
+      console.warn('⚠️ mAddress invalid, skipping balance check:', mAddr);
+      return;
+    }
+
+    try {
+      this.web3 = new Web3(
+        new Web3.providers.HttpProvider('http://127.0.0.1:7545')
+      );
+
+      this.web3.eth.getBalance(mAddr, function (error, resultWei) {
+        if (!error) {
+          const diffEth = Number(app.web3.utils.fromWei(resultWei, 'ether'));
+          if (diffEth >= amount) {
+            clearInterval(app.intervalHandle);
+            clearInterval(app.intervalBalance);
+          }
+          app.setState({ diff: diffEth });
+        } else {
+          console.error('❌ Error checking balance:', error);
+        }
+      });
+    } catch (err) {
+      console.error('❌ Web3 error in bCheck():', err);
+    }
+  }
+
+  // =============== Bắt đầu bộ đếm & kiểm tra thanh toán ===================
+  startTimer() {
+    if (this.state.tflag === true) {
+      const time = parseInt(this.state.minutes, 10) || 0;
+      this.secondsRemaining = time * 60;
+
+      this.intervalHandle = setInterval(this.tick, 1000);
+      this.intervalBalance = setInterval(this.bCheck, 10000);
+
+      this.setState({ tflag: false });
+    }
+  }
+
+  // =============== Lúc khởi chạy app ===================
+  componentDidMount() {
+    const shoes = Shoes.map(({ logo, price, image, name }) => ({
+      logo,
+      price,
+      image,
+      name,
+    }));
+    this.setState({ shoes });
+  }
+
+  // =============== RENDER UI ===================
+  render() {
+    return (
+      <div>
+        <Nav appName={this.appName} />
+        <Description />
+
+        {this.state.errorMsg && (
+          <div className="notification is-danger" style={{ margin: '12px' }}>
+            {this.state.errorMsg}
+          </div>
+        )}
+
+        <Container
+          shoes={this.state.shoes}
+          newPayment={this.newPayment}
+          closePayment={this.closePayment}
+          PaymentDetail={this.state.PaymentDetail}
+          mAddress={this.state.mAddress}
+          amount={this.state.amount}
+          diff={this.state.diff}
+          paymentf={this.state.paymentf}
+          Conv={this.state.Conv}
+          MMaskTransfer={this.MMaskTransfer}
+          PaymentWait={this.PaymentWait}
+          startTimer={this.startTimer}
+          tick={this.tick}
+          defaultGasPrice={this.state.defaultGasPrice}
+          defaultGasLimit={this.state.defaultGasLimit}
+          minutes={this.state.minutes}
+          seconds={this.state.seconds}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
